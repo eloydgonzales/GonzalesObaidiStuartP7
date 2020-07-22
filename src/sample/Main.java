f944baa159326cf7a5b12421e5180ab51e5915b9
mspace00 <47034030+mspace00@users.noreply.github.com>
2020-07-22 12:02:12 -0600

    header, no user resize, program name in title
    Added header comment
    Set correct program name and title in window
    Forced window to be fixed (no user resize)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,3 +1,10 @@
+/**************************
+ * Michael Stuart         *
+ * mstuart3@cnm.edu       *
+ * GonzalesStuartObaidiP7 *
+ * Main.java              *
+ * ************************/
+
 package sample;
 
 import javafx.application.Application;
@@ -11,8 +18,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("GonzalesStuartObaidiP7");
+        primaryStage.setTitle("GonzalesObaidiStuartP7 - 3 Card Brag"); //MS added program title
         primaryStage.setScene(new Scene(root, 650, 500));
+        primaryStage.setResizable(false); //MS don't allow user to resize window
         primaryStage.show();
     }
 
